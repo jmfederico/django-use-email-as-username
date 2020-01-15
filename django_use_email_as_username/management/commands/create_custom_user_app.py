05bb22cc4d5fec5ecec86b5296b05cbe0a6aec4e
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2020-01-15 11:56:19 -0500

    Fix Django 3 compatibility

--- django_use_email_as_username/management/commands/create_custom_user_app.py
+++ django_use_email_as_username/management/commands/create_custom_user_app.py
@@ -9,7 +9,11 @@ from django.core.management.templates import BaseCommand
 class Command(BaseCommand):
     """Define create_emailuser_app command."""
 
-    help = "Creates a Django app hat subclasses the User model declared by Django Use Email as Username."
+    requires_system_checks = False
+    help = (
+        "Creates a Django app hat subclasses the User model declared by Django Use"
+        " Email as Username."
+    )
 
     def add_arguments(self, parser):
         """Define arguments for create_emailuser_app command."""
@@ -23,8 +27,10 @@ class Command(BaseCommand):
             "directory", nargs="?", help="Optional destination directory"
         )
 
-    def handle(self, **options):
+    def handle(self, name, **options):
         """Call "startapp" to generate app with custom user model."""
-        template = os.path.dirname(os.path.abspath(__file__)) + "/app_template"
-        name = options.pop("name")
-        call_command("startapp", name, template=template, **options)
+        options["template"] = (
+            os.path.dirname(os.path.abspath(__file__)) + "/app_template"
+        )
+        options.pop("skip_checks")
+        call_command("startapp", name, **options)
--- tests/settings.py
+++ tests/settings.py
@@ -1,30 +1,42 @@
 """Minimum settings to run tests."""
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7a%8!3)_=_c04_i@ai1yfw=fz&gf6(b5vp6(@_#h4&n9276vjj'
+SECRET_KEY = "7a%8!3)_=_c04_i@ai1yfw=fz&gf6(b5vp6(@_#h4&n9276vjj"
 
 DEBUG = True
 
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.admin',
-    'django.contrib.contenttypes',
-
-    'django_use_email_as_username.apps.DjangoUseEmailAsUsernameConfig',
-    'custom_user_test.apps.CustomUserTestConfig',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django_use_email_as_username.apps.DjangoUseEmailAsUsernameConfig",
+    "custom_user_test.apps.CustomUserTestConfig",
 ]
 
-MIDDLEWARE = []
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
 
 ROOT_URLCONF = "tests.urls"
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    }
-}
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
 
 USE_TZ = True
 
-AUTH_USER_MODEL = 'custom_user_test.User'
+AUTH_USER_MODEL = "custom_user_test.User"
